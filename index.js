33dd9f399251c646399a006d991fc4a610bd1b6c
Sean Clifford <sclifford6262@gmail.com>
2018-05-07 18:23:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,11 @@
    BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
+
    BoardMember.prototype.doCharity = function(){
      return "I like to help people."
 
+   }
    BoardMember.prototype.releasePressStatement = function(){
      return "You will see great things from Scuber."
    }
